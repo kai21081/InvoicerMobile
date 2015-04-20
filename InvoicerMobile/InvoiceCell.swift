08c061fba434749a5985591141953de8127305e1
Jung Kim <jung.jung.k@gmail.com>
2015-04-20 11:45:01 -0700

    first commit

---
+++ InvoicerMobile/InvoiceCell.swift
@@ -0,0 +1,24 @@
+//
+//  InvoiceCell.swift
+//  InvoicerMobile
+//
+//  Created by Jisoo Hong on 2015. 4. 20..
+//  Copyright (c) 2015년 JHK. All rights reserved.
+//
+
+import UIKit
+
+class InvoiceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
