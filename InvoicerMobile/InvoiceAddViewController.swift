4ada37ea69557343ced23d786902077e3ad3977d
Craig Chaillie <k1e1g1@yahoo.com>
2015-04-21 11:05:11 -0700

    Added adaptiveTextFieldViewController

--- InvoicerMobile/InvoiceAddViewController.swift
+++ InvoicerMobile/InvoiceAddViewController.swift
@@ -2,13 +2,13 @@
 //  InvoiceAddViewController.swift
 //  InvoicerMobile
 //
-//  Created by Jisoo Hong on 2015. 4. 20..
-//  Copyright (c) 2015년 JHK. All rights reserved.
+//  Created by User on 2015. 4. 20..
+//  Copyright (c) 2015 Craig Chaillie. All rights reserved.
 //
 
 import UIKit
 
-class InvoiceAddViewController: UIViewController {
+class InvoiceAddViewController: AdaptiveTextFieldViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
