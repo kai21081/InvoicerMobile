a070eae3f340b4562de07257d33a43611b9adb37
lazersly <brandonjroberts@yahoo.com>
2015-04-22 10:22:05 -0700

    Added currency extension for NSNumber.  Added gradients class for making backgrounds.  Changed InvoiceView to have gradient background and white text.  Fixed transition issue from Login to InvoiceView

--- InvoicerMobile/InvoiceCell.swift
+++ InvoicerMobile/InvoiceCell.swift
@@ -3,6 +3,7 @@
 //  InvoicerMobile
 //
 //  Created by Jisoo Hong on 2015. 4. 20..
+//  Written by Brandon Roberts 4/20/15
 //  Copyright (c) 2015년 JHK. All rights reserved.
 //
 
@@ -23,6 +24,10 @@ class InvoiceCell: UITableViewCell {
     self.amountLabel.text = nil
     self.invoicePaidIndicatorView.image = nil
     
+    // Make color changes
+    let white = UIColor.whiteColor()
+    self.nameLabel.textColor = white
+    self.amountLabel.textColor = white
   }
   
   override func setSelected(selected: Bool, animated: Bool) {
---
+++ InvoicerMobile/UIColorExtension.swift
@@ -0,0 +1,55 @@
+//
+//  UIColorExtension.swift
+//  InvoicerMobile
+//
+//  Created by Brandon Roberts on 4/21/15.
+//  Copyright (c) 2015 JHK. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  
+  // Can't get RANGE to work in Swift
+  
+//  class func colorWithHexString(hexString: String) -> UIColor? {
+//    
+//    var cString = hexString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).uppercaseString
+//    
+//    if (count(cString) != 6) { return nil }
+//    
+//    if (cString.hasPrefix("0X") || cString.hasPrefix("0x")) {
+//      cString = cString.substringFromIndex(advance(cString.startIndex, 2))
+//    }
+//    
+////    let range = NSMakeRange(0, 2)
+//    
+//    var range = Range(start:cString.startIndex,end:cString.endIndex)
+//    let rString = cString.substringWithRange(range)
+//
+//    
+//    // Separate into r, g, b substrings
+//    NSRange range;
+//    range.location = 0;
+//    range.length = 2;
+//    NSString *rString = [cString substringWithRange:range];
+//    
+//    range.location = 2;
+//    NSString *gString = [cString substringWithRange:range];
+//    
+//    range.location = 4;
+//    NSString *bString = [cString substringWithRange:range];
+//    
+//    // Scan values
+//    unsigned int r, g, b;
+//    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+//    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+//    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+//    
+//    return [UIColor colorWithRed:((float) r / 255.0f)
+//      green:((float) g / 255.0f)
+//      blue:((float) b / 255.0f)
+//      alpha:1.0f];
+//  }
+
+}
