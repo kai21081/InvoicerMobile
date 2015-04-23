b090d7083d4c1dfb73b452abf903c9958b89b5fd
lazersly <brandonjroberts@yahoo.com>
2015-04-23 10:59:28 -0700

    Changed add view, attempted to add spinner, changed some colors, implementing API integration

--- InvoicerMobile/StripeService.swift
+++ InvoicerMobile/StripeService.swift
@@ -10,13 +10,19 @@ import Foundation
 
 class StripeService {
   
-  let localHostURL = NSURL(string: "http://127.0.0.1:3000/")!
+  let localHostString = "http://127.0.0.1:3000/"
+  let invoiceReApiPrefixString = "https://www.invoice.re/api/v1/"
   
   func fetchInvoicesForCompany(companyID : String, completionHandler: ([Invoice]?)->Void) {
 //    let query = "?KEYS=VALUES"
 //    let url = NSURL(string: localHost + query)
     
-    let request = NSURLRequest(URL: localHostURL)
+    let companyInvoiceURL = NSURL(string: invoiceReApiPrefixString + "invoices")
+    
+    let token = NSUserDefaults.standardUserDefaults().valueForKey(kUserDefaultsStripeTokenKey) as! String
+    
+    let request = NSMutableURLRequest(URL: companyInvoiceURL!)
+    request.setValue(token, forHTTPHeaderField: "stripe-access-token")
     
     let requestTask = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
       if error == nil {
