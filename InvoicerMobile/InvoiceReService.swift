377644cf6e16e47716cd389782bf99226124ce21
Craig Chaillie <k1e1g1@yahoo.com>
2015-04-24 12:28:55 -0700

    Implemented email request code in detailVC and in invoiceReService.

--- InvoicerMobile/InvoiceReService.swift
+++ InvoicerMobile/InvoiceReService.swift
@@ -112,7 +112,24 @@ class InvoiceReService {
     }
   }
   
+  class func requestEmailToBeSent(invoiceID: String, completionHandler:(Bool) ->()) {
+    let urlString = "https://www.invoice.re/api/v1/invoices/" + invoiceID + "/send_invoice"
+    var emailRequest = NSMutableURLRequest(URL: NSURL(string: urlString)!)
+    emailRequest.HTTPMethod = "POST"
+    let dataTask = NSURLSession.sharedSession().dataTaskWithRequest(emailRequest, completionHandler: { (data, response, error) -> Void in
+      if let httpResponse = response as? NSHTTPURLResponse {
+        println("\(httpResponse.statusCode)")
+        if httpResponse.statusCode == 200  {
+          completionHandler(true)
+        } else {
+          completionHandler(false)
+        }
+      }
+    })
+    dataTask.resume()
+  }
   
+
   class func fetchInvoicesForCompany(companyID : String, completionHandler: ([Invoice]?)->Void) {
     
     let localHostString = "http://127.0.0.1:3000/"
